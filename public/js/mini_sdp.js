7a4a84ac83b9afe4aab1f59a3df35ac25062b118
warmer <ken.kinion@gmail.com>
2015-04-09 09:10:14 -0400

    Adding comments clarifying the fields that are saved and passed to the peer

--- public/js/mini_sdp.js
+++ public/js/mini_sdp.js
@@ -52,10 +52,37 @@ var send_channel;
 function sdp_info(sdp) {
   var sdp_info = {};
 
+  // Notes per https://tools.ietf.org/html/rfc5245
+  // ice-ufrag and ice-pwd contain up to 256 'ice-char's [a-zA-z0-9/\+]
+  // ice-ufrag will contain at least 4 'ice-char's
+  // ice-pwd will contain at least 22 'ice-char's
+  // each 'ice-char' contains 6 bits of data, so 192 bytes worst-case
+  // would be needed to store those fields, plus an additional 5 bits for
+  // the length of the ice-ufrag field (though possible ranges are limited
+  // to 29 values: [4-32], and 4 bits to store the ice-pwd field length
+  // (since possible ranges are limited to 11 values: [22-32]
   sdp_info.ice_ufrag =    sdp.match(/^a=ice-ufrag:(.*)$/m)[1];
   sdp_info.ice_pwd =      sdp.match(/^a=ice-pwd:(.*)$/m)[1];
+  // the fingerprint contains ASCII hex bytes separated by ':'
+  // for sha-256 string is 32 bytes, thus (32 * 3 - 1) = 95 chars long
+  // Notes per https://tools.ietf.org/html/rfc4572
+  // "sha-256" is hard-coded here, but could theoretically be any of:
+  // sha-1 sha-224 sha-256 sha-384 sha-512 md5 md2
+  // Research indicates that we might be able to limit this to
+  // sha-256 sha-384 and sha-512
   sdp_info.fingerprint =  sdp.match(/^a=fingerprint:sha-256 (.*)$/m)[1];
+  // setup is one of either 'actpass' or 'active' and thus just 1 bit
   sdp_info.setup =        sdp.match(/^a=setup:(.*)$/m)[1];
+  // each candidate contains the following information:
+  // foundation (1-32 'ice-char's) - receiver may not care if hard-coded
+  // component-id (1-5 digits) - receiver probably doesn't care
+  // transport ('udp' or 'tcp') - 1 bit needed
+  // priority (could probably be hard-coded, maybe unique)
+  // ip address (4 bytes assuming ipv4)
+  // port (2 bytes)
+  // candidate type (the text 'typ ' + one of [host srflx prflx relay]
+  // the text 'tcptyp active' when 'tcp' is the transport
+  // the text 'generation 0'
   sdp_info.candidates =   sdp.match(/^a=candidate:(.*)$/gm);
 
   return sdp_info;
